57a578effbdee13d027144531a615c06259ad1b1
Luc Mainguy <luc.mainguy1@gmail.com>
2022-08-16 19:40:47 -0500

    JS Drumkit: Implement audio on keypress

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,4 +1,9 @@
-const keys = document.querySelector('.keys');
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return; //stop the function
+    audio.play();
+});
+//const keys = document.querySelector('.keys');
 
-console.log(keys.data)
+//console.log(keys.data)
 //keys.addEventListener('click', )
