c5ac1aba1dc4a704370f780660ebfeb1c4140e69
Luc Mainguy <luc.mainguy1@gmail.com>
2022-08-16 20:25:37 -0500

    JS Drumkit: Complete

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,8 +1,20 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; //stop the function
+    audio.currentTime = 0;
     audio.play();
+    key.classList.add('playing');
 });
+
+function removeTransition(e) {
+    //if(e.propertyName !== 'transform') return;
+    console.log(e);
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionEnd', removeTransition));
+
 //const keys = document.querySelector('.keys');
 
 //console.log(keys.data)
