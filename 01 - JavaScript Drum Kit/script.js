af8a4d9cc7d5f13bc491bd3de2794e8fe3630a2d
Luc Mainguy <luc.mainguy1@gmail.com>
2022-08-17 08:21:40 -0500

    JS Drumkit: migrate js to external file

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,21 +1,21 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return; //stop the function
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-});
 
-function removeTransition(e) {
-    //if(e.propertyName !== 'transform') return;
-    console.log(e);
+//window.onload is needed to get eventlisteners working in external js. see https://stackoverflow.com/questions/42341761/javascript-eventlistener-not-working-in-external-js-file 
+window.onload = function() {
+window.addEventListener('keydown', playSound);
+    function playSound(e) {
+      const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+      const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+      if(!audio) return; //stop the function
+      audio.currentTime = 0;
+      audio.play();
+      key.classList.add('playing');
+    }
+  
+  function removeTransition(e) {
+      if(e.propertyName !== 'transform') return;
+      this.classList.remove('playing');
+  }
+  
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 }
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionEnd', removeTransition));
-
-//const keys = document.querySelector('.keys');
-
-//console.log(keys.data)
-//keys.addEventListener('click', )
